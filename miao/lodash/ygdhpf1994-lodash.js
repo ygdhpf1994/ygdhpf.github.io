8aa013df07f19bcd14781fedd3c7cbad35fec4b7
wei <1270253125@qq.com>
2019-07-19 22:04:10 +0800

    1

--- miao/lodash/ygdhpf1994-lodash.js
+++ miao/lodash/ygdhpf1994-lodash.js
@@ -2,4 +2,13 @@ var ygdhpf1994 = {
     compact: function(ary) {
         return ary.filter(it => it)
     },
+    chunk: function(ary,size = 1){
+        var a = []
+        while(ary.length !== 0){
+            if(ary.length >= size){
+                a[i] = []
+                a[i].push()
+            }
+        }
+    },
 }
