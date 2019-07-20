3ed3245fec76515f30eee86320f0f03e9ac6be4c
wei <1270253125@qq.com>
2019-07-20 11:30:26 +0800

    2

--- miao/lodash/ygdhpf1994-lodash.js
+++ miao/lodash/ygdhpf1994-lodash.js
@@ -5,10 +5,39 @@ var ygdhpf1994 = {
     chunk: function(ary,size = 1){
         var a = []
         while(ary.length !== 0){
-            if(ary.length >= size){
-                a[i] = []
-                a[i].push()
+            a.push(ary.splice(0,size))
+        }
+        return a
+    },
+    difference: function(ary,values){
+        var res = []
+        for(var i = 0 ; i < ary.length ; i ++){
+            if(values.indexOf(ary[i]) === -1){
+                res.push(ary[i])
             }
         }
+        return res
     },
+    differenceBy: function(ary , ...test) {
+        
+        var key = test[test.length - 1]
+        var rest = test.slice(0,test.length - 1)
+        if(typeof key === 'function'){
+            rest.pop()
+            var temp = []
+            var p = temp.concat(...rest).map(key)
+            var q = ary.map(key)
+            return difference(q, p)
+        } else if(typeof key === 'string'){
+            rest.pop()
+            var temp = []
+            var p = temp.concat(...rest)
+            return difference(ary , p)
+        } else {
+            return difference(ary,rest)
+        }
+    },
+    
 }
+
+        
